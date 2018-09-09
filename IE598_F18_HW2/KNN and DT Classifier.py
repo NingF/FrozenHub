ad694b8fd115c8472ed85221bddc759facc73f55
FrozenFire <qd_fanning@sina.com>
2018-09-08 23:06:33 -0500

    Add files via upload

---
+++ IE598_F18_HW2/KNN and DT Classifier.py
@@ -0,0 +1,118 @@
+#Our first machine learning model
+#Garreta and Moncecchi pp 10-20
+#uses Iris database and SGD classifier
+import sklearn
+print( 'The scikit learn version is {}.'.format(sklearn.__version__))
+
+from sklearn import datasets
+iris = datasets.load_iris()
+X_iris, y_iris = iris.data, iris.target
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import metrics
+from sklearn.cross_validation import train_test_split
+from sklearn import preprocessing
+# Get dataset with only the first two attributes
+X, y = X_iris[:, :2], y_iris
+# Split the dataset into a training and a testing set
+# Test set will be the 25% taken randomly
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.25, random_state=33)
+# Standardize the features
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+import matplotlib.pyplot as plt
+
+
+############K-NN Classifier###############
+from sklearn.neighbors import KNeighborsClassifier
+
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 26)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors=k)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train,y_train)
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train, y_train)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+########We can see that K=12 might be the best choice of K here####################
+
+###########plot_decision_regions########################
+from matplotlib.colors import ListedColormap
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot all samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],alpha=0.8, c=cmap(idx),marker=markers[idx], label=cl)   
+ 
+
+############Decision Tree Classifier####################
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier(criterion='entropy',max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+
+#Plot all data
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier=tree, test_idx=range(105,150))
+plt.xlabel('petal length')
+plt.ylabel('petal width') 
+plt.legend(loc='upper left')
+plt.show()
+#Plot test data only
+plot_decision_regions(X_test, y_test, classifier=tree, test_idx=range(105,150))
+plt.xlabel('petal length')
+plt.ylabel('petal width') 
+plt.legend(loc='upper left')
+plt.show()
+
+print("My name is Ning Fan")
+print("My NetID is: ningfan2")
+print("I hereby certify that I have read the University policy on Academic Integrity and that I am not in violation.")
+######STOP HERE######################
+
+
+
+
+
+
+
+
+
+
